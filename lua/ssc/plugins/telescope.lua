1b19afcad2361c20166c1f8913e44779bb39dd7d
Scr0w <nulltillasn@gmail.com>
2024-12-09 23:13:28 -0300

    update some maps telescope

--- lua/ssc/plugins/telescope.lua
+++ lua/ssc/plugins/telescope.lua
@@ -4,7 +4,6 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
-
     config = function()
         require('telescope').setup({
             pickers = {
@@ -15,19 +14,21 @@ return {
         })
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        -- FIND THOSE FILES 
+        vim.keymap.set('n', '<leader>fs', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
+        vim.keymap.set('n', '<leader>fws', function()
+        vim.keymap.set('n', '<leader>fg', function()
+            builtin.grep_string({ search = vim.fn.input("Grep: ") })
+        end)
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>pWs', function()
+        vim.keymap.set('n', '<leader>fWs', function()
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        -- Get some help
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
