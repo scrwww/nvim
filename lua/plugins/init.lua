563632be0b6d97d0ff41b131c29013ce78243ef5
scrwww <119636547+scrwww@users.noreply.github.com>
2026-06-14 13:46:16 -0300

    added oil and transparency

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,8 +4,13 @@ vim.pack.add({
         { src = "https://github.com/nvim-lua/plenary.nvim" },
         { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
 
+        { src = "https://github.com/stevearc/oil.nvim" },
+
         -- theme
-        { src = "https://github.com/oskarnurm/koda.nvim" },
+        { src = "https://github.com/xiyaowong/transparent.nvim" },
+        { src = "https://github.com/ntk148v/komau.vim" },
+        -- { src = "https://github.com/Alligator/accent.vim" },
+        -- -- { src = "https://github.com/romainl/Apprentice" },
 
         -- lsp and utils
         { src = "https://github.com/nvim-treesitter/nvim-treesitter",          version = "main" },
@@ -19,3 +24,4 @@ vim.pack.add({
 require("plugins.telescope")
 require("plugins.devicons")
 require("plugins.lsp")
+require("plugins.oil")
